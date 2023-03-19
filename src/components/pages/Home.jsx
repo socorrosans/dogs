0ce2376c81d5ab6af1fc20ef281bc2d7bd4ae2e8
Socorro Santos <socorro.sans1@gmail.com>
2023-03-19 02:51:31 -0300

    acessing photos endpoint with fetch - page home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,35 @@
-import {Container, Head} from '../bosons';
+import {Container, Head, Grid} from '../bosons';
 import {Header, Footer} from '../organisms';
+import { useState, useEffect } from 'react';
+import {api} from '../../service/api';
 
 export default function Home(){
+
+  const [photos, setPhotos] = useState([]);
+
+  async function getPhotos(){
+    await fetch(`${api}/api/photo`, {
+      method: 'GET'
+    }).then(response => response.json())
+      .then(json => setPhotos(json));
+  }
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
   return(
    <>
     <Head title="Fotos | Dogs"/>
     <Header/>
     <Container>
+      <Grid>
+        {
+          (photos.length !== 0) ? photos.map(photo => {
+            return <div key={photo.id}><img src={photo.src} alt={`Foto de ${photo.title}`}/></div>
+          }) : null
+        }
+      </Grid>
     </Container>
     <Footer/>
    </>
